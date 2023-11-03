af4189d851c019726e6574aa3b547896faf2e0bc
SharminAkterr <sharminakter3e@gmail.com>
2023-11-03 17:02:53 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let num = 0;
+let intervalId;
+
+
+const startHandler = () => {
+    const buttonOutputDiv = document.getElementById('button-output');
+
+    // Clear the previous interval if it exists
+    clearInterval(intervalId);
+
+    buttonOutputDiv.textContent = '';
+    const p = document.createElement('p');
+    buttonOutputDiv.appendChild(p);
+
+    intervalId = setInterval(() => {
+        p.innerText = ++num;
+        console.log(num);
+    }, 1000);
+
+}
+
+const stopHandler = () => {
+    clearInterval(intervalId);
+}
+
+const restartHandler = () => {
+    num = 0
+    const p = document.querySelector('#button-output p');
+    p.innerText = num;
+    console.log(num)
+}
